0197d6d16d637fa1cd880454fbebe6575e9e1e80
Kevin Kim <kimkevin7704@gmail.com>
2022-10-12 15:22:39 -0700

    sample script info

--- sample-script.sh
+++ sample-script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Prints text to screen
+# Author: Kevin Kim
+# Date Created: 10/11/2022
+# Last Modified: 10/12/2022
+
+# Description
+# prints "Hello World!" to terminal
+
+# Usage
+# sample-script.sh
 
 echo "Hello World!"
-exit 0
