12b6f631495f14a36617124124db2b03e2b3e9b8
Kevin Kim <kimkevin7704@gmail.com>
2022-10-12 15:27:46 -0700

    backup script added

---
+++ backup-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Author: Kevin Kim
+# Date Created: 10/12/2022
+# Last Modified: 10/12/2022
+
+# Description
+# backs up files in DevOpsPlayground directory in a .tar archive
+
+# Usage
+# backup-script.sh
+
+tar -cvf /DevOpsPlayground/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
